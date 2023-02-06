c751ac8adca8e72dc018064c550a1cbb486319c5
Mario Stura <mxs9672@g.rit.edu>
2023-02-06 23:21:20 +0000

    Add index.php

---
+++ public_html/index.php
@@ -0,0 +1,32 @@
+RuvfaCXXV3QCGS3sC36byjziwUEzYBa1yq
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <title>Project Homepage</title>
+
+</head>
+
+<body>
+
+    <div>This should be the front controller that routes all incoming requests to the appropriate controller.</div>
+
+
+
+    <div>
+
+        <?php
+
+        echo 'Today’s date is <b>' . date('Y/m/d') . '</b> and it’s a <b>' . date('l') . '</b> today!';
+
+        ?>
+
+    </div>
+
+
+
+</body>
+
+</html>
