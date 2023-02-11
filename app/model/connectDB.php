15515a17a4e5f9498e7af916957e6a911d3e8137
Mario Stura <mxs9672@g.rit.edu>
2023-02-11 04:52:11 +0000

    DB test

--- app/model/connectDB.php
+++ app/model/connectDB.php
@@ -13,6 +13,6 @@ try {
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
 } catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+    die("ERROR: Could not connect. " . $e->getMessage());
 }
 ?>
