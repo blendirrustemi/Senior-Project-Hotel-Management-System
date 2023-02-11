9ab4cac8bdb2fa58e65edd4805a1e180e5ffb487
Mario Stura <mxs9672@g.rit.edu>
2023-02-11 04:14:06 +0000

    Add script to connect php to MariaDB server

---
+++ app/controller/connectDB.php
@@ -0,0 +1,18 @@
+<?php
+
+use PDO;
+
+// Connect to MariaDB
+$servername = "your_server_name";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database_name";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
