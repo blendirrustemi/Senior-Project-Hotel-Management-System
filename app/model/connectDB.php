40a3dcac43d393b976525b60d8dd842629efd7c8
Denim Behn <denim.behn@afrozaar.com>
2023-02-25 10:00:59 +0100

    Revert "[Feature] MVC usage and CRUD"
    This reverts commit 2823ce2b72334e6622201ad2f8829c278ba3929c.

--- app/model/connectDB.php
+++ app/model/connectDB.php
@@ -1,22 +1,17 @@
 <?php
 
+use PDO;
+
 // Connect to MariaDB
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "yin_db";
+$servername = "your_server_name";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database_name";
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
-    
-    $stmt = $conn->query("SELECT * FROM room_type");
-    while ($row = $stmt->fetch()) {
-        echo $row['room_type_name']."<br />\n";
-    }
-    
-    
 } catch(PDOException $e) {
     die("ERROR: Could not connect. " . $e->getMessage());
 }
