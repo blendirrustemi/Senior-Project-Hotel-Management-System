daa554a4321a10edb222204a93a50cedb4596809
Mario Stura <mxs9672@g.rit.edu>
2023-03-20 05:06:56 +0100

    Add app folder with MVC project files

---
+++ app/core/config.php
@@ -0,0 +1,9 @@
+<?php
+if($_SERVER['SERVER_NAME'] == 'localhost')
+{
+    define('ROOT', 'http://localhost/seniorProject/public/');
+
+}else{
+
+    define('ROOT', 'https://www.yinnsitethatwillbeused.com');
+}
