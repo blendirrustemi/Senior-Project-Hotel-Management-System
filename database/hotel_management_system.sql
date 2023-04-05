ab108e743da002413fc9f880ad7046c2dd5faeda
Blendi Rrustemi <br3433@g.rit.edu>
2023-04-05 12:42:15 +0000

    Populated the Database

--- database/hotel_management_system.sql
+++ database/hotel_management_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 05, 2023 at 02:22 PM
+-- Generation Time: Apr 05, 2023 at 02:41 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -24,6 +24,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `Bookings`
+--
+
+CREATE TABLE `Bookings` (
+  `BookingID` int(11) NOT NULL,
+  `CustomerID` int(11) DEFAULT NULL,
+  `RoomID` int(11) DEFAULT NULL,
+  `CheckInDate` date DEFAULT NULL,
+  `CheckOutDate` date DEFAULT NULL,
+  `Adults` int(11) DEFAULT NULL,
+  `Children` int(11) DEFAULT NULL,
+  `Requests` text DEFAULT NULL,
+  `NightsToStay` int(11) DEFAULT NULL,
+  `TotalPrice` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `Bookings`
+--
+
+INSERT INTO `Bookings` (`BookingID`, `CustomerID`, `RoomID`, `CheckInDate`, `CheckOutDate`, `Adults`, `Children`, `Requests`, `NightsToStay`, `TotalPrice`) VALUES
+(1, 2, 1, '2023-05-01', '2023-05-05', 2, 0, 'Non-smoking room', 4, '920.00'),
+(2, 3, 2, '2023-05-06', '2023-05-13', 2, 1, 'Extra towels', 7, '1750.00');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `Customers`
 --
 
@@ -38,14 +65,72 @@ CREATE TABLE `Customers` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `Customers`
+--
+
+INSERT INTO `Customers` (`CustomerID`, `FirstName`, `LastName`, `Email`, `Phone`, `Password`, `isAdmin`) VALUES
+(1, 'Blendi', 'Rrustemi', 'blendirrustemi@example.com', '123-456-7890', 'blendi321', 1),
+(2, 'Mario', 'Stura', 'mariostura@example.com', '987-654-3210', 'mario321', 0),
+(3, 'Denim', 'Behn', 'denimbehn@example.com', '987-654-3210', 'denim321', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Rooms`
+--
+
+CREATE TABLE `Rooms` (
+  `RoomID` int(11) NOT NULL,
+  `RoomNumber` varchar(20) DEFAULT NULL,
+  `RoomType` varchar(50) DEFAULT NULL,
+  `Description` text DEFAULT NULL,
+  `PricePerNight` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `Rooms`
+--
+
+INSERT INTO `Rooms` (`RoomID`, `RoomNumber`, `RoomType`, `Description`, `PricePerNight`) VALUES
+(1, '101', 'Standard', 'The Blue Room is a cozy and comfortable option for solo travelers or couples.', '230.00'),
+(2, '201', 'Standard', 'The Bolero Room is a spacious and elegant choice, perfect for a romantic getaway.', '250.00'),
+(3, '301', 'Suite', 'The Rose Suite is a luxurious option with a king-size bed, a private balcony, and stunning views of the surrounding area.', '230.00'),
+(4, '401', 'Suite', 'The Lodge Suite is the ultimate in comfort and style, featuring a private living room, a fireplace, and a hot tub.', '230.00');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `Bookings`
+--
+ALTER TABLE `Bookings`
+  ADD PRIMARY KEY (`BookingID`),
+  ADD KEY `CustomerID` (`CustomerID`),
+  ADD KEY `RoomID` (`RoomID`);
 
 --
 -- Indexes for table `Customers`
 --
 ALTER TABLE `Customers`
   ADD PRIMARY KEY (`CustomerID`);
+
+--
+-- Indexes for table `Rooms`
+--
+ALTER TABLE `Rooms`
+  ADD PRIMARY KEY (`RoomID`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Bookings`
+--
+ALTER TABLE `Bookings`
+  ADD CONSTRAINT `bookings_ibfk_1` FOREIGN KEY (`CustomerID`) REFERENCES `Customers` (`CustomerID`),
+  ADD CONSTRAINT `bookings_ibfk_2` FOREIGN KEY (`RoomID`) REFERENCES `Rooms` (`RoomID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
