10aa73108aa0fd4b7dc8867fd4e75625a74517e5
Denim Behn <denim.behn@afrozaar.com>
2023-04-20 14:28:52 +0200

    [Styling] Floated admin add buttons to right

--- app/views/users.view.php
+++ app/views/users.view.php
@@ -94,7 +94,7 @@ if (isset($_POST['search_btn'])){
             <div class="admin-recent-card">
                 <div class="title">Admins</div><br>
 
-                <button class="add_admin_button" style="background-color: #4CAF50; color: white; font-size: 16px; padding: 10px 20px; border: none; border-radius: 4px; box-shadow: 0 2px 2px rgba(0, 0, 0, 0.3); margin-bottom: 10px;">
+                <button class="add_admin_button" style="background-color: #4CAF50; color: white; font-size: 16px; padding: 10px 20px; border: none; border-radius: 4px; box-shadow: 0 2px 2px rgba(0, 0, 0, 0.3); margin-bottom: 10px; float: right;">
                     <a href="addadmin" style="color:white; text-decoration: none;">Add an Admin</a>
                 </button>
 
